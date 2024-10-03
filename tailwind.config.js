c5114deef5dc94a86eb5bf435c6f4811019141df
Clementine Digout <clementine.digout@yahoo.fr>
2024-10-03 12:05:44 -0400

    add tailwind config + more html

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ export default {
     "./src/**/*.{js,html}",
   ],
   theme: {
+    colors: {
+      'black': '#191A18',
+      'grey': '#5E615D',
+      'gree n': '#B7D08E',
+      'blue': '#98DAEA',
+      'salmon': '#F2BD95',
+      'purple': '#D2CEFF',
+      'offWhite': '#FDFCFA',
+      'transWhite': '#FDFCFA[.5]'
+    },
+    fontFamily: {
+      'sans': ['Roboto', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
 }
-
